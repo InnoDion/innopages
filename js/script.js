2f2963614975e3c369588732f82fa6769711c4a0
InnoDion <jasondcentr307@gmail.com>
2026-04-15 09:55:36 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+}
+
+function showMessage() {
+    document.getElementById("message").innerText = "You clicked the button!";
+}
